d2cea3094fcd5b446a34f05f344555069f55fcb8
Akash <aakashsoni8756@gmail.com>
2024-10-08 11:55:20 +0530

    Projects updated..

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const about = {
     description:
         "Hello! I'm Akash Soni, a dedicated and passionate Full Stack Java Developer. With a strong foundation in both frontend and backend technologies, I thrive on creating seamless and efficient web applications.",
     resume:
-        'https://drive.google.com/file/d/1s2gKQ0FHByMOoXQNCQZ9CpI8OJIUmhkE/view?usp=drive_link',
+        'https://drive.google.com/file/d/1o8N1Urnx8CdZ9kuiB09OiSKwbkC3B9mT/view',
 
     social: {
         linkedin: 'https://www.linkedin.com/in/akash-soni-6312aa1a1/',
@@ -18,6 +18,14 @@ const about = {
 }
 
 const projects = [
+    {
+        name: 'Social Media Login Application',
+        description:
+            'Developed a comprehensive social media login application using React.js and Firebase. This project features a seamless authentication process, allowing users to log in using multiple methods, including email/password, Google, Facebook, Twitter, and GitHub.',
+        stack: ['React JS, Firebase'],
+        sourceCode: 'https://github.com/aakash-sony/React-Practice/tree/social_media_login',
+        livePreview: 'https://github.com/aakash-sony/React-Practice/tree/social_media_login',
+    },
     {
         name: 'Kanban Board',
         description:
@@ -35,6 +43,14 @@ const projects = [
         livePreview: 'https://aakash-sony.github.io/react_project01_TextUtil/',
     },
     {
+        name: 'Book Store Management',
+        description:
+            'Developed a web-based bookstore management application using Servlets and JSP. Features include user and admin authentication, user registration, and book management capabilities for admins (add, view, update, delete books). Users can browse and purchase books, with session management for login/logout. ',
+        stack: ['Servlet, JSP'],
+        sourceCode: 'https://github.com/aakash-sony/Book-store/tree/master',
+        livePreview: 'https://github.com/aakash-sony/Book-store/tree/master',
+    },
+    {
         name: 'Phone Directory',
         description:
             'This project manages contact list. You can add, delete, update and retrieve the contact. Custom header files have been created and used in this project.',
@@ -50,14 +66,6 @@ const projects = [
         sourceCode: 'https://github.com/aakash-sony/_authentication',
         livePreview: 'https://github.com/aakash-sony/_authentication',
     },
-    {
-        name: 'To Do List',
-        description:
-            'This is an authentication based web application.You can maintain your day to day activities using this application.The major features of this application is registration, login, update profile, change password, forget password, add todo date- wise and priority.You can mark the status as done, in progress, on hold, on rejected etc.',
-        stack: ['HTML, CSS, JAVASCRIPT'],
-        sourceCode: 'https://github.com/aakash-sony/todo_app',
-        livePreview: 'https://github.com/aakash-sony/todo_app',
-    },
 ]
 
 const skills = [
@@ -65,7 +73,10 @@ const skills = [
     'CSS',
     'JavaScript',
     'React Js',
-    'Java',
+    'Core Java',
+    'Advance Java',
+    'Spring Core',
+    'Spring Boot',
     'Oracle',
     'GitHub',
 
